936143e0a3551de1267791d06cf6cd615ed8cde4
joyung0402 <164139663+joyung0402@users.noreply.github.com>
2024-08-22 08:17:33 +0000

    Deploying to gh-pages from @ joyung0402/startupfun-front@fd2d4c0625a84242841eaeb74f5513a817f73517 🚀

---
+++ assets/ssrBoot-WWII7APK.js
@@ -0,0 +1,1 @@
+import{K as t,S as e,c as s,b2 as n}from"./index-BKZHNWHs.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
